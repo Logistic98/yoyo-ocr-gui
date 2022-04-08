a3854aceab6c6b4476df3e16799dbae8fb1f53da
Eula <5355251+jun98@user.noreply.gitee.com>
2022-04-08 12:35:37 +0800

    initialize project

---
+++ PrScrn.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+from time import sleep
+from PIL import Image, ImageGrab
+from ctypes import windll
+
+
+def screenshot(img_path):
+
+    if windll.user32.OpenClipboard(None):
+        windll.user32.EmptyClipboard()
+        windll.user32.CloseClipboard()
+
+    os.system('start /B rundll32 PrScrn.dll PrScrn')
+
+    # 等待截图后放到剪切板
+    index = 0
+    im = ImageGrab.grabclipboard()
+    while not im:
+        if index < 500:
+            im = ImageGrab.grabclipboard()
+            sleep(0.01)
+            index = index + 1
+        else:
+            break
+
+    if isinstance(im, Image.Image):
+        im.save(img_path)
+
---
+++ README.md
@@ -0,0 +1,25 @@
+## yoyo-ocr-gui
+
+内容说明：见我的博客 [使用PySide6图形界面框架制作GUI](https://www.eula.club/使用PySide6图形界面框架制作GUI.html)
+
+项目结构：
+
+```
+yoyo-ocr-gui
+ ├── config.ini
+ ├── logo
+ │   ├── logo.ico
+ │   └── logo.png
+ ├── PrScrn.dll
+ ├── PrScrn.py
+ ├── ui
+ │   └── Ui_YoyoOCR.ui
+ ├── gol.py
+ ├── Ui_YoyoOCR.py
+ └── YoyoOCR.py
+```
+
+功能界面：
+
+![YoyoOCR美化后的样式](https://chevereto.eula.club/images/2022/04/05/1b64efc5ed74.png)
+
